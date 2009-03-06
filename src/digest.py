59cbd98b4ba87bc98a489197874994f4f21ea711
D. Allan Drummond <dadrummond@gmail.com>
2009-03-05 18:22:03 -0600

    better splitting algorithm

--- src/digest.py
+++ src/digest.py
@@ -1,18 +1,48 @@
 import sys, os, math, string
 import biofile
 
+def split(seq, pat):
+	frags = []
+	s = seq
+	prev_pos = -1
+	pos = s.find(pat, prev_pos+1)
+	while pos > -1:
+		print pat, prev_pos, pos
+		frags.append(s[prev_pos+1:pos+1])
+		prev_pos = pos
+		pos = s.find(pat, prev_pos+1)
+	if prev_pos > 0:
+		frags.append(s[prev_pos+1:])
+	if prev_pos == -1 and pos == -1:
+		# No matches at all
+		frags.append(seq)
+	return frags
+		
+
 def digest(seq, patterns, complete=True):
-	for pat in patterns:
-		frags = seq.split(pat)
-		print frags
-		if seq.startswith(pat):
-			pass
+	all_frags = []
+	digest = [seq]
+	if complete:
+		for pat in patterns:
+			new_digest = []
+			for frag in digest:
+				frags = split(frag, pat) #["%s%s" % (p,pat) for p in frag.split(pat)]
+				# if seq.startswith(pat):
+				#	frags[0] = "%s%s" % (frags[0], pat)
+				if frag.endswith(pat):
+					frags = frags[:-1]
+				new_digest += frags
+			digest = new_digest
+	return digest
 
 if __name__ == '__main__':
 	fname = sys.argv[1]
 	patterns = sys.argv[2].split("/")
 	(headers, seqs) = biofile.readFASTA(os.path.expanduser(fname))
 	for seq in seqs:
-		digest(seq, patterns, True)
+		frags = digest(seq, patterns, True)
+		print "\n%s\n---" % seq
+		for f in frags:
+			print f
 	
 
