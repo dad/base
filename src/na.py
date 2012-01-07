956c1b607aa570bd8b0485c4987bd5d76f3d98fc
D. Allan Drummond <dadrummond@gmail.com>
2012-01-06 18:25:07 -0600

    putting NA functions in a place where they can be used cheaply by stats

---
+++ src/na.py
@@ -0,0 +1,39 @@
+import math
+
+# Definition of the default NA string.
+NA = 'NA'
+
+def isNA(x):
+	res = x is None
+	if not res:
+		if type(x) == str:
+			x = x.upper()
+			res = (x==NA or x=='' or x=='NAN')
+		elif type(x) == float:
+			res = math.isnan(x)
+	return res
+
+def strNA(x):
+	if isNA(x):
+		return na.NA
+	else:
+		return str(x)
+
+def formatNA(x, format="{0}", sep=None):
+	if isinstance(x, list):
+		flds = format.split(sep)
+		if sep is None:
+			sep = '\t'
+		assert len(flds) == len(x)
+		res = []
+		for i in range(len(x)):
+			if isNA(x[i]):
+				res.append(na.NA)
+			else:
+				res.append(flds[i].format(x[i]))
+		return sep.join(res)
+	else:
+		if isNA(x):
+			return na.NA
+		else:
+			return format.format(x)
