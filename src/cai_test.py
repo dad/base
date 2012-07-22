c26cf3303a1bd0fb8c071ed1800dd1874841d82e
D. Allan Drummond <dadrummond@gmail.com>
2012-07-22 18:58:10 -0500

    new testing file

---
+++ src/cai_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+"""Module for testing codon bias module "cai".
+"""
+
+import re, os, sys, string, math, random, unittest
+import cai
+
+if __name__=='__main__':
+	unittest.main(verbose=2)
+
