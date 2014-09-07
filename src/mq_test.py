b4ab304d1d6fd8d22ed3997ba39b2dd5c75d36ab
D. Allan Drummond <dadrummond@uchicago.edu>
2014-09-07 08:34:09 -0500

    maxquant tests

---
+++ src/mq_test.py
@@ -0,0 +1,11 @@
+import time, os, random, string, sys, math, traceback, unittest
+import util, na, mq
+
+
+class test_(unittest.TestCase):
+	"""sample test"""
+	def test_run(self):
+		self.assertTrue(True)
+
+if __name__=="__main__":
+	unittest.main(verbosity=2)
