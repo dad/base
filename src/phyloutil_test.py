b8d659c0f55d17ddfa2565bd681917f709d8e1b9
D. Allan Drummond <dadrummond@gmail.com>
2016-09-08 15:44:14 -0500

    buildTree works properly

--- src/phyloutil_test.py
+++ src/phyloutil_test.py
@@ -1,14 +1,15 @@
 import time, os, random, string, sys, math, traceback, unittest
-from Bio.Phylo import Newick
+from Bio.Phylo import Newick, BaseTree
 import phyloutil
 
-def buildTree(depth, breadth, parent):
-	for n in range(depth):
+def buildTree(depth, breadth, parent, id):
+	if depth>0:
 		for i in range(breadth): # binary
 			node = Newick.Clade()
-			node.name = "{}.{}".format(n,i)
-			node.parent = parent
-			buildTree(depth-1, breadth, node)
+			node.name = "{}.{}".format(id,i+1)
+			parent.clades.append(node)
+			print("Added node level {}".format(depth))
+			buildTree(depth-1, breadth, node, node.name)
 
 
 
@@ -16,10 +17,11 @@ class test001(unittest.TestCase):
 	def test_search(self):
 		"""Find by name"""
 		root = Newick.Clade()
-		root.parent = None
-		buildTree(3,2,root)
-		print(root)
-
+		#root.parent = None
+		root.name = 'root'
+		buildTree(3,2,root,'root')
+		for n in BaseTree._level_traverse(root, lambda x: x.clades):
+			print(n)
 		#self.assertTrue(geneutil.longestRun('AAAAA','A')==5)
 
 
