5470e59f1d65f3e22782a7a05d4772a7eba3335a
D. Allan Drummond <dadrummond@gmail.com>
2016-09-09 00:31:17 -0500

    all tests pass -- new printing, identification of a bug (?) in draw_ascii where a linear tree cannot be rendered

--- src/phyloutil.py
+++ src/phyloutil.py
@@ -2,6 +2,16 @@ import sys
 from Bio import Phylo
 from Bio.Phylo import Newick
 
+def printTree(root, out, indent='', perlevel='  ', iterating=False):
+	if not iterating:
+		out.write("\n")
+	outstr = ''
+	if not root.name is None:
+		outstr = root.name
+	out.write(indent+outstr+"\n")
+	for clade in root.clades:
+		printTree(clade, out, indent+perlevel, iterating=True)
+
 
 def findNodeByName(name, node):
 	"""Find node or child whose node.name matches name."""
@@ -21,15 +31,22 @@ def parseClassificationTable(root, table):
 	cur_child = None
 	#Phylo.write(root, sys.stdout, 'newick')
 	for name in names:
+		#print(name)
 		node = Newick.Clade()
 		node.name = name
 		node_found_in_tree = findNodeByName(node.name, root)
 		if not cur_child is None:
+			#Phylo.draw_ascii(root)
 			if not node_found_in_tree is None:
 				# Insert the children into the tree, and exit
 				node_found_in_tree.clades.append(cur_child)
+				#print("Node {} found\n".format(node.name))
 				break
 			else:
 				node.clades.append(cur_child)
+		else:
+			# This is the leaf node
+			#node.
+			pass
 		cur_child = node
 	#Phylo.write(cur_child, sys.stdout, 'newick')
--- src/phyloutil_test.py
+++ src/phyloutil_test.py
@@ -28,21 +28,54 @@ class test001(unittest.TestCase):
 		target = phyloutil.findNodeByName(target_name, root)
 		self.assertTrue(target.name == target_name)
 
-	def test_ascii(self):
-		"""Find by name"""
-		root = Newick.Clade()
-		#root.parent = None
-		root.name = 'root'
-		buildTree(3,2,root,'root')
-		#Phylo.draw_ascii(root)
-
 	def test_reading_from_class_table(self):
 		"""Read table"""
 		root = Newick.Clade()
+		root.name = "cellular organisms"
 		inf = open("./test-phyloutil/test1/Pseudozyma-antarctica-1.txt", 'r')
 		table = util.readTable(inf, header=True)
 		#print(table)
 		phyloutil.parseClassificationTable(root, table)
 		inf.close()
+		#phyloutil.printTree(root, sys.stdout)
+		termlist = list(root.get_terminals())
+		self.assertTrue(termlist[0].name=='Moesziomyces antarcticus T-34')
+
+	def test_reading_from_guide_table(self):
+		"""Read table"""
+		root = Newick.Clade()
+		root.name = "cellular organisms"
+		#print(root.depths())
+		inf = open("./test-phyloutil/test1/Pseudozyma-antarctica-1.txt", 'r')
+		table = util.readTable(inf, header=True)
+		#print(table)
+		phyloutil.parseClassificationTable(root, table)
+		inf.close()
+		phyloutil.printTree(root, sys.stdout)
+		#print(root.depths())
+		#Phylo.draw_ascii(root)
+
+	def test_simple_manual_tree(self):
+		"""Manually constructed tree"""
+		root = Newick.Clade()
+		root.name = "root"
+		child = Newick.Clade()
+		child.name = "kid1"
+		root.clades.append(child)
+		child = Newick.Clade()
+		child.name = "kid2"
+		root.clades.append(child)
+		phyloutil.printTree(root, sys.stdout)
+		#Phylo.draw_ascii(root)
+
+	def test_print(self):
+		"""Print tree"""
+		root = Newick.Clade()
+		#root.parent = None
+		root.name = 'root'
+		buildTree(3,2,root,'root')
+		phyloutil.printTree(root, sys.stdout)
+		print(root)
+
 if __name__=="__main__":
 	unittest.main(verbosity=2)
