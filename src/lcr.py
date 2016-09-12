61e30257d2d4ded1cfa01b578a4f7689373aa9dd
D. Allan Drummond <dadrummond@uchicago.edu>
2016-09-11 21:50:10 -0500

    new files for studying constraints on low-complexity regions (LCRs)

---
+++ src/lcr.py
@@ -0,0 +1,10 @@
+import sys
+from Bio import Phylo
+from Bio.Phylo import Newick
+from Bio.Phylo import NewickIO
+from io import StringIO
+
+'''
+Tests and utility functions for low-complexity regions (LCRs).
+'''
+
